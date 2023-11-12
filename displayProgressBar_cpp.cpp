00ee192e288b0262b695c702a0723331d795560d
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-11-11 19:35:55 -0500

    Create displayProgressBar_cpp.cpp

---
+++ displayProgressBar_cpp.cpp
@@ -0,0 +1,16 @@
+void displayProgressBar(float progress, int barWidth = 50, std::string line="=")
+	{
+		std::cout << "[";
+		int pos = static_cast<int>(progress * barWidth);
+		for (int i = 0; i < barWidth; ++i) 
+			{
+				if (i < pos)
+					{std::cout << line;}
+				else if (i == pos)
+					{std::cout << ">";}
+				else
+					{std::cout << " ";}
+			}
+		std::cout << "] " << static_cast<int>(progress * 100.0)<< "%\r";
+		std::cout.flush();
+	}	
